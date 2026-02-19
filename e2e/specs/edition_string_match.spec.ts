959bdbd823548772ec3df619ed224e4ea0eef73d
Daniel Elsner <daniel.elsner@oeaw.ac.at>
2026-02-19 13:42:30 +0000

    adding DetailInfo Type; minor build

--- e2e/specs/edition_string_match.spec.ts
+++ e2e/specs/edition_string_match.spec.ts
@@ -2,20 +2,23 @@
 /* eslint-disable playwright/no-conditional-in-test */
 import { expect, test } from "@playwright/test";
 
-// import type { TestDataObjectComment } from "../../src/interfaces.js";
 import { fetchTestData } from "../utils/helpers.js";
+
+type DetailInfo = {
+    id: string;
+    description: string;
+    description_long: string;
+}
 
 const pathToTest = "D_000002-003-000";
 
-const testData: Array<any> = await fetchTestData(`${pathToTest}.json`);
+const testData: Array<DetailInfo> = await fetchTestData(`${pathToTest}.json`);
 
 testData.forEach((detail_info) => {
     test(`testing edition page for ${pathToTest} (${detail_info.id})`, async ({ page }) => {
         await page.goto(`http://localhost:8000/html/${pathToTest}.html`);
 
         const long_desc_element = page.getByTestId(detail_info.id);
-        console.log("Test", long_desc_element);
-        // console.log(detail_info.description_long);
         await expect(long_desc_element).toHaveText(detail_info.description_long);
 
     });
